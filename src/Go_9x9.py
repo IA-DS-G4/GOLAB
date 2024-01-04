1dfb08a098a41450d147e0082bf4c2d887025077
EliasDSAI <elias.88348@gmail.com>
2024-01-04 23:46:50 +0000

    Muzero is running now for Go7x7

--- src/Go_9x9.py
+++ src/Go_9x9.py
@@ -1,122 +1,53 @@
-import datetime
-import math
-import pathlib
 import numpy
-import torch
-from .go_board import GoBoard
-from .go_utils import GoUtils
-from .abstract_game import AbstractGame
+from go_board import GoBoard
+from go_utils import GoUtils
+from muzeroconfig import MuZeroConfig, ActionHistory
+from typing import List
+from Wrappers import Action, Player, Node
+import tensorflow as tf
 
 
-class MuZeroConfig:
-    def __init__(self):
-        # fmt: off
-        # More information is available here: https://github.com/werner-duvaud/muzero-general/wiki/Hyperparameter-Optimization
+class Go9x9Config(MuZeroConfig):
 
-        self.seed = 0  # Seed for numpy, torch and the game
-        self.max_num_gpus = None  # Fix the maximum number of GPUs to use. It's usually faster to use a single GPU (set it to 1) if it has enough memory. None will use every GPUs available
+    def new_game(self):
+        return Go9x9()
 
-        ### Game
-        self.observation_shape = (3, 9, 9)  # Dimensions of the game observation, must be 3 (channel, height, width). For a 1D array, please reshape it to (1, 1, length of array)
-        self.action_space = list(range(-1,(9 * 9)))  # Fixed list of all possible actions. You should only edit the length
-        self.players = list(range(2))  # List of players. You should only edit the length
-        self.stacked_observations = 0  # Number of previous observations and previous actions to add to the current observation
-        self.action_space_size = 9 * 9
+def make_Go9x9_config() -> MuZeroConfig:
+    def visit_softmax_temperature(num_moves, training_steps):
 
-        # Evaluate
-        self.muzero_player = 0  # Turn Muzero begins to play (0: MuZero plays first, 1: MuZero plays second)
-        self.opponent = "random"  # Hard coded agent that MuZero faces to assess his progress in multiplayer games. It doesn't influence training. None, "random" or "expert" if implemented in the Game class
+        # higher temperature higher exploration
 
-        ### Self-Play
-        self.num_workers = 2  # Number of simultaneous threads/workers self-playing to feed the replay buffer
-        self.selfplay_on_gpu = False
-        self.max_moves = 121  # Maximum number of moves if game is not finished before
-        self.num_simulations = 400  # Number of future moves self-simulated
-        self.discount = 1  # Chronological discount of the reward
-        self.temperature_threshold = None  # Number of moves before dropping the temperature given by visit_softmax_temperature_fn to 0 (ie selecting the best action). If None, visit_softmax_temperature_fn is used every time
-
-        # Root prior exploration noise
-        self.root_dirichlet_alpha = 0.3
-        self.root_exploration_fraction = 0.25
-
-        # UCB formula
-        self.pb_c_base = 19652
-        self.pb_c_init = 1.25
-
-        ### Network
-        self.network = "resnet"  # "resnet" / "fullyconnected"
-        self.support_size = 10  # Value and reward are scaled (with almost sqrt) and encoded on a vector with a range of -support_size to support_size. Choose it so that support_size <= sqrt(max(abs(discounted reward)))
-
-        # Residual Network
-        self.downsample = False  # Downsample observations before representation network, False / "CNN" (lighter) / "resnet" (See paper appendix Network Architecture)
-        self.blocks = 6  # Number of blocks in the ResNet
-        self.channels = 128  # Number of channels in the ResNet
-        self.reduced_channels_reward = 2  # Number of channels in reward head
-        self.reduced_channels_value = 2  # Number of channels in value head
-        self.reduced_channels_policy = 4  # Number of channels in policy head
-        self.resnet_fc_reward_layers = [64]  # Define the hidden layers in the reward head of the dynamic network
-        self.resnet_fc_value_layers = [64]  # Define the hidden layers in the value head of the prediction network
-        self.resnet_fc_policy_layers = [64]  # Define the hidden layers in the policy head of the prediction network
-
-        # Fully Connected Network
-        self.encoding_size = 32
-        self.fc_representation_layers = []  # Define the hidden layers in the representation network
-        self.fc_dynamics_layers = [64]  # Define the hidden layers in the dynamics network
-        self.fc_reward_layers = [64]  # Define the hidden layers in the reward network
-        self.fc_value_layers = []  # Define the hidden layers in the value network
-        self.fc_policy_layers = []  # Define the hidden layers in the policy network
-
-        ### Training
-        self.results_path = pathlib.Path(__file__).resolve().parents[1] / "results" / pathlib.Path(
-            __file__).stem / datetime.datetime.now().strftime(
-            "%Y-%m-%d--%H-%M-%S")  # Path to store the model weights and TensorBoard logs
-        self.save_model = True  # Save the checkpoint in results_path as model.checkpoint
-        self.training_steps = 10000  # Total number of training steps (ie weights update according to a batch)
-        self.batch_size = 512  # Number of parts of games to train on at each training step
-        self.checkpoint_interval = 50  # Number of training steps before using the model for self-playing
-        self.value_loss_weight = 1  # Scale the value loss to avoid overfitting of the value function, paper recommends 0.25 (See paper appendix Reanalyze)
-        self.train_on_gpu = torch.cuda.is_available()  # Train on GPU if available
-
-        self.optimizer = "SGD"  # "Adam" or "SGD". Paper uses SGD
-        self.weight_decay = 1e-4  # L2 weights regularization
-        self.momentum = 0.9  # Used only if optimizer is SGD
-
-        # Exponential learning rate schedule
-        self.lr_init = 0.002  # Initial learning rate
-        self.lr_decay_rate = 0.9  # Set it to 1 to use a constant learning rate
-        self.lr_decay_steps = 10000
-
-        ### Replay Buffer
-        self.replay_buffer_size = 10000  # Number of self-play games to keep in the replay buffer
-        self.num_unroll_steps = 121  # Number of game moves to keep for every batch element
-        self.td_steps = 121  # Number of steps in the future to take into account for calculating the target value
-        self.PER = True  # Prioritized Replay (See paper appendix Training), select in priority the elements in the replay buffer which are unexpected for the network
-        self.PER_alpha = 0.5  # How much prioritization is used, 0 corresponding to the uniform case, paper suggests 1
-
-        # Reanalyze (See paper appendix Reanalyse)
-        self.use_last_model_value = False  # Use the last model to provide a fresher, stable n-step value (See paper appendix Reanalyze)
-        self.reanalyse_on_gpu = False
-
-        ### Adjust the self play / training ratio to avoid over/underfitting
-        self.self_play_delay = 0  # Number of seconds to wait after each played game
-        self.training_delay = 0  # Number of seconds to wait after each training step
-        self.ratio = 1  # Desired training steps per self played step ratio. Equivalent to a synchronous version, training can take much longer. Set it to None to disable it
-        # fmt: on
-
-    def visit_softmax_temperature_fn(self, trained_steps):
-        """
-        Parameter to alter the visit count distribution to ensure that the action selection becomes greedier as training progresses.
-        The smaller it is, the more likely the best action (ie with the highest visit count) is chosen.
-
-        Returns:
-            Positive float.
-        """
-        if trained_steps < 0.5 * self.training_steps:
-            return 1.0
-        elif trained_steps < 0.75 * self.training_steps:
+        if training_steps < 100:
+            return 3
+        elif training_steps < 125:
+            return 2
+        elif training_steps < 150:
+            return 1
+        elif training_steps < 175:
             return 0.5
+        elif training_steps < 200:
+            return 0.250
+        elif training_steps < 225:
+            return 0.125
+        elif training_steps < 250:
+            return 0.075
         else:
-            return 0.25
+            return 0.001
+
+    return Go9x9Config(action_space_size= 82,
+                        observation_space_size= 81,
+                        observation_space_shape= (9,9),
+                        max_moves=500,
+                        discount=0.997,
+                        dirichlet_alpha=0.25,
+                        num_simulations=150,
+                        batch_size=100,
+                        td_steps=20,
+                        lr_init=0.0001,
+                        lr_decay_steps=5000,
+                        training_episodes=225,
+                        hidden_layer_size= 49,
+                        visit_softmax_temperature_fn=visit_softmax_temperature)
 
 
 class Go9x9:
@@ -124,11 +55,15 @@ class Go9x9:
         self.board_size = 9
         self.player = 1 # Black goes first
         self.board = GoBoard(board_dimension=self.board_size, player=self.player)
-        self.utils = GoUtils
-
-    def to_play(self):
-        return 0 if self.player == 1 else 1
-
+        self.utils = GoUtils()
+        self.observation_space_shape = (self.board_size,self.board_size)
+        self.observation_space_size = self.board_size**2
+        self.action_space_size = (self.board_size**2)+1
+        self.action_history_list = []
+        self.rewards = []
+        self.child_visits = []
+        self.root_values = []
+        self.discount = 0.997
     def reset(self):
         self.player = 1
         self.board = GoBoard(board_dimension=self.board_size, player=self.player)
@@ -138,7 +73,7 @@ class Go9x9:
         r = numpy.floor(action / self.board_size)
         c = action % self.board_size
         move = (r,c)
-        if action == -1:
+        if action == self.board_size**2:
             move = (-1,-1)
         self.utils.make_move(board=self.board,move=move)
         done = self.utils.is_game_finished(board=self.board)
@@ -149,32 +84,79 @@ class Go9x9:
         self.player *= -1
         return self.get_observation(), reward, done
 
-    def get_observation(self):
-        board_player1 = numpy.where(self.board.board_grid == 1, 1.0, 0.0)
-        board_player2 = numpy.where(self.board.board_grid == -1, 1.0, 0.0)
-        board_to_play = numpy.full((self.board_size,self.board_size), self.player, dtype="int32")
-        return numpy.array([board_player1, board_player2, board_to_play])
+    def apply(self, action: Action):
 
-    def legal_actions(self):
-        # Pass (-1, -1) as a valid move
-        legal = [-1]
+        observation, reward, done = self.step(action.index)
+        self.rewards.append(reward)
+        self.action_history_list.append(action)
+
+
+
+    def get_observation(self):
+        return tf.constant([self.board.board_grid],dtype="int32")
+
+    def legal_actions(self)-> List[Action]:
+        # Pass = boardsize**2 is always legal
+        legal = [self.board_size**2]
         for i in range(self.board_size):
             for j in range(self.board_size):
                 if self.utils.is_valid_move(board=self.board,move=(i,j)):
                     legal.append(i * self.board_size + j)
-        return legal
+        return [Action(index) for index in legal]
+
+    def total_rewards(self):
+
+        return sum(self.rewards)
 
     def is_finished(self):
         return self.utils.is_game_finished(board=self.board)
 
-    def render(self):
-        pass
+    def to_play(self):
+        return Player(self.board.player)
 
-    def human_input_to_action(self):
-        pass
-        return 1
+    def action_history(self) -> ActionHistory:
 
-    def action_to_human_input(self, action):
-        pass
-        return 1
+        return ActionHistory(self.action_history_list, self.action_space_size,self.board.player)
 
+    def store_search_statistics(self, root: Node):
+
+        sum_visits = sum(child.visit_count for child in root.children.values())
+        action_space = (Action(index) for index in range(self.action_space_size))
+        self.child_visits.append([
+            root.children[a].visit_count / sum_visits if a in root.children else 0
+            for a in action_space
+        ])
+        self.root_values.append(root.value())
+
+    def make_target(self, state_index: int, num_unroll_steps: int, td_steps: int, to_play: Player,
+                    action_space_size: int):
+
+        # The value target is the discounted root value of the search tree N steps
+        # into the future, plus the discounted sum of all rewards until then.
+        targets = []
+        for current_index in range(state_index, state_index + num_unroll_steps + 1):
+            bootstrap_index = current_index + td_steps
+            if bootstrap_index < len(self.root_values):
+                value = self.root_values[bootstrap_index] * self.discount ** td_steps
+            else:
+                value = 0
+
+            for i, reward in enumerate(self.rewards[current_index:bootstrap_index]):
+                value += reward * self.discount ** i  # pytype: disable=unsupported-operands
+
+            if current_index > 0 and current_index <= len(self.rewards):
+                last_reward = self.rewards[current_index - 1]
+            else:
+                last_reward = None
+
+            if current_index < len(self.root_values):
+                targets.append((value, last_reward, self.child_visits[current_index]))
+            else:
+                # States past the end of games are treated as absorbing states.
+                targets.append((0, last_reward, []))
+
+        return targets
+
+
+if __name__ == "__main__":
+    pass
