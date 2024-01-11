2ac7568b6b657ecf31e8bbcf047b42d6b42b4123
Diederik60 <diederikdekker@hotmail.nl>
2024-01-11 19:53:32 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -112,7 +112,7 @@ def update_weights(optimizer: tf.keras.optimizers.Optimizer, network: Network, b
 def train_network(config: MuZeroConfig, storage: SharedStorage, replay_buffer: ReplayBuffer, iterations: int):
     network = storage.latest_network()
     learning_rate = config.lr_init * config.lr_decay_rate ** (iterations / config.lr_decay_steps)
-    optimizer = tf.keras.optimizers.legacy.Adam()
+    optimizer = keras.optimizers.Adam()
 
 
     batch = replay_buffer.sample_batch(config.num_unroll_steps, config.td_steps, config.action_space_size)
@@ -126,7 +126,7 @@ def train_network(config: MuZeroConfig, storage: SharedStorage, replay_buffer: R
         network.save_network_deepcopy(model_name=config.model_name)
 
     storage.save_network(network)
-    tf.keras.backend.clear_session()
+    keras.backend.clear_session()
 
     return loss
 
